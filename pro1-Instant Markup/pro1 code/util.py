a24c5cf9f8fb45511fbbbb5afba6c473c3caa022
Essense <movebeyondfear@gmail.com>
2017-11-19 01:50:46 +0800

    Create util.py

---
+++ pro1-Instant Markup/pro1 code/util.py
@@ -0,0 +1,13 @@
+import sys
+def lines(file):
+    for line in file: yield line
+    yield '\n'
+        
+def blocks(file):
+    block = []
+    for line in lines(file):
+        if line.strip():
+            block.append(line)
+        elif block:
+            yield ''.join(block).strip()
+            block = []
